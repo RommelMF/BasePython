0dd0fe5bb0bd9d6db703ac4c995202df906d4824
roman.mostafin <roman.mostafin@factory5.ai>
2021-03-19 22:08:30 +0300

    Fix functions

--- homework_01/main.py
+++ homework_01/main.py
@@ -16,18 +16,15 @@ PRIME = "prime"
 
 def filter_numbers(args, type):
     predicates = {
-        "odd": lambda x: x % 2 == 0,
-        "even": lambda x: x % 2 != 0,
-        "prime": define_prime_number
+        "odd": lambda x: x % 2 != 0,
+        "even": lambda x: x % 2 == 0,
+        "prime": is_prime
     }
     return list(filter(predicates[type], args))
 
 
-def define_prime_number(num):
+def is_prime(num):
     value = 2
     while num % value != 0:
         value += 1
     return value == num
-
-# val = power_numbers(2,4)
-# print()
