1f41800f11b8d5cc99e23cf543b563a715e32b64
roman.mostafin <roman.mostafin@factory5.ai>
2021-03-22 20:16:23 +0300

    Ref: move predicates from filter_numbers

--- homework_01/main.py
+++ homework_01/main.py
@@ -14,17 +14,19 @@ EVEN = "even"
 PRIME = "prime"
 
 
-def filter_numbers(args, type):
-    predicates = {
-        "odd": lambda x: x % 2 != 0,
-        "even": lambda x: x % 2 == 0,
-        "prime": is_prime
-    }
-    return list(filter(predicates[type], args))
-
-
 def is_prime(num):
     value = 2
     while num % value != 0:
         value += 1
     return value == num
+
+
+predicates = {
+        "odd": lambda x: x % 2 != 0,
+        "even": lambda x: x % 2 == 0,
+        "prime": is_prime
+    }
+
+
+def filter_numbers(args, type):
+    return list(filter(predicates[type], args))
