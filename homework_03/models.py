2b3b728cb90aad933789c4b0befc0c8374fbd36a
roman.mostafin <roman.mostafin@factory5.ai>
2021-06-12 20:28:03 +0300

    Fix: logging

--- homework_03/models.py
+++ homework_03/models.py
@@ -24,7 +24,7 @@ from sqlalchemy import (
     ForeignKey,
 )
 
-PG_CONN_URI = os.environ.get("SQLALCHEMY_PG_CONN_URI") or "postgresql+asyncpg://user:password@localhost:8002/project"
+PG_CONN_URI = os.environ.get("SQLALCHEMY_PG_CONN_URI") or "postgresql+asyncpg://postgres:password@localhost/postgres"
 
 
 engine = create_async_engine(PG_CONN_URI, echo=True)
