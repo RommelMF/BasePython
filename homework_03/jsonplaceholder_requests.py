3fa7f03ba87de2f4a67813953e2274253b2f8b40
roman.mostafin <roman.mostafin@factory5.ai>
2021-06-12 14:19:44 +0300

    Fix: logging

--- homework_03/jsonplaceholder_requests.py
+++ homework_03/jsonplaceholder_requests.py
@@ -8,12 +8,11 @@ USERS_DATA_URL = "https://jsonplaceholder.typicode.com/users"
 POSTS_DATA_URL = "https://jsonplaceholder.typicode.com/posts"
 
 
-logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
-
 @dataclass
 class Service:
     name: str
     url: str
+
 
 SERVICES = [
     Service("users", USERS_DATA_URL),
@@ -27,18 +26,12 @@ async def fetch_json(session: ClientSession, url: str) -> dict:
 
 
 async def fetch_ip(service: Service) -> str:
-    logging.info(f'Fetch ip from {service.name}')
-
     async with ClientSession() as session:
         result = await fetch_json(session, service.url)
-
-    logging.info(f'Fetched {service.name}. Result: {result}')
-
     return result
 
 
 async def get_users() -> str:
-    logging.info("Start getting my users")
     task_result = ""
 
     tasks = {
@@ -55,20 +48,15 @@ async def get_users() -> str:
 
     for task in pending:
         task.cancel()
-        logging.info(f"Cancelled task {task}")
 
     for task in done:
         task_result = task.result()
         break
-    else:
-        logging.warning("Could not fetch users!")
 
-    logging.info("Finish getting my users")
     return task_result
 
 
 async def get_posts() -> str:
-    logging.info("Start getting my posts")
     task_result = ""
 
     tasks = {
@@ -85,23 +73,9 @@ async def get_posts() -> str:
 
     for task in pending:
         task.cancel()
-        logging.info(f"Cancelled task {task}")
 
     for task in done:
         task_result = task.result()
         break
-    else:
-        logging.warning("Could not fetch posts!")
 
-    logging.info("Finish getting my posts")
     return task_result
-
-def main():
-    users = asyncio.run(get_users())
-    posts = asyncio.run(get_posts())
-    logging.info(f"RESULT: {users}")
-    logging.info(f"RESULT: {posts}")
-
-
-if __name__ == '__main__':
-    main()
