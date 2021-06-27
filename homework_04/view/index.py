7fa7001a96ccf673e5ad00c18eb01695c8e0dbee
roman.mostafin <roman.mostafin@factory5.ai>
2021-06-27 10:18:14 +0300

    Add: homework_04

---
+++ homework_04/view/index.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, request, jsonify, render_template, url_for, redirect
+
+
+index_app = Blueprint('index_app', __name__, url_prefix='/')
+
+@index_app.route("/about", endpoint='about')
+def about():
+    return render_template('index/about.html')
+
+@index_app.route("/", endpoint='home')
+def home():
+    return render_template('index/base.html')
