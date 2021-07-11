ebffb04efd735d8dac26452866016fdc666e58f1
RommelMF <dragondrum@mail.ru>
2021-07-11 16:31:58 +0300

    Add: ListView, DetailView, CreateView, UpdateView, DeleteView

---
+++ homework_08/interesting_places/places/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=32, unique=True)
+    description = models.TextField(blank=True)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Place(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    description = models.TextField(blank=True)
+    rate = models.FloatField(default=0.0)
+    img = models.ImageField(upload_to='places', blank=True, null=True)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
